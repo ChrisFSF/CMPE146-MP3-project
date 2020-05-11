7320e09873c03bcb78d3ac8d31a65d288ffe62e5
Chi-Ho <wch1923@hotmail.com>
2020-05-10 17:16:01 -0700

    Update accel_control.c
    Now the accelerometer control is working as intended

--- sjtwo-c/projects/lpc40xx_freertos/l5_application/accel_control.c
+++ sjtwo-c/projects/lpc40xx_freertos/l5_application/accel_control.c
@@ -16,68 +16,29 @@
 
 uint8_t acc_interpret_input(uint8_t Button_Status){
   acceleration__axis_data_s id = acceleration__get_data();
-  if (id.z < 800){
-    if (-400 < id.x  && id.x < 400){
-      if      (id.y > 700)  { Button_Status = status_read_button_up;
-                              OLED__DEBUG_PRINTF("ACC_Up\n"); }
-      else if (id.y < -800) { Button_Status = status_read_button_down;
-                              OLED__DEBUG_PRINTF("ACC_Down\n"); }
+  if (id.z < 950){  // when tilted more than 30 degree, 1023 * cos(30) = 886
+    if (3750 < id.x  || id.x < 350){ 
+      if      (350 < id.y && id.y < 1000) { Button_Status = status_read_button_up;
+                                            OLED__DEBUG_PRINTF("ACC_Up\n"); }
+      else if (3100 < id.y && id.y < 3750){ Button_Status = status_read_button_down;
+                                            OLED__DEBUG_PRINTF("ACC_Down\n"); }
     }
-    else if (-400 < id.y && id.y < 400){
-      if      (id.x < -800) { Button_Status = status_read_button_left;
-                              OLED__DEBUG_PRINTF("ACC_Left\n"); }
-      else if (id.x > 800)  { Button_Status = status_read_button_right;
-                              OLED__DEBUG_PRINTF("ACC_Right\n"); }
+    else if (3750 < id.y || id.y < 350){
+      if      (350 < id.x && id.x < 1000) { Button_Status = status_read_button_right;
+                                            OLED__DEBUG_PRINTF("ACC_Right\n"); }
+      else if (3100 < id.x && id.x < 3750){ Button_Status = status_read_button_left;
+                                            OLED__DEBUG_PRINTF("ACC_Left\n"); }
     }
-    else if ((400 < id.x ) && (400 < id.y )) {
-      Button_Status = status_read_button_option;
-      OLED__DEBUG_PRINTF("ACC_Option\n");
+    else if (350 < id.y && id.y < 1000){
+      if      (350 < id.x  && id.x < 1000){ Button_Status = status_read_button_option;
+                                            OLED__DEBUG_PRINTF("ACC_Option\n"); }
+      else if (3100 < id.x && id.x < 3750){ Button_Status = status_read_button_back;
+                                            OLED__DEBUG_PRINTF("ACC_Exit\n"); }
     }
-    else if ((id.x < -400) && (400 < id.y)) {
-      Button_Status = status_read_button_back;
-      OLED__DEBUG_PRINTF("ACC_Exit\n");
-    }
-    else if ((400 < id.x ) && (id.y < -400)) {
-      Button_Status = status_read_button_play_pause;
-      OLED__DEBUG_PRINTF("ACC_Play\n");
-    }
+    else if (350 < id.x  && id.x < 1000 && 
+             3100 < id.y && id.y < 3750)  { Button_Status = status_read_button_play_pause;
+                                            OLED__DEBUG_PRINTF("ACC_Play\n"); }
   delay__ms(100);
   }
   return Button_Status;
 }
-
-// uint8_t acc_interpret_input(){
-//   uint8_t Button_Status;
-//   acceleration__axis_data_s id = acceleration__get_data();
-
-//   if ((-400 < id.x < 400) && (id.y > 700) && (id.z < 800)) {
-//     Button_Status = status_read_button_up;
-//   }
-
-//   else if ((-400 < id.x < 400) && (id.y < -800) && (id.z < 800)) {
-//     Button_Status = status_read_button_down;
-//   }
-
-//   else if ((id.x < -800) && (-400 < id.y < 400) && (id.z < 800)) {
-//     Button_Status = status_read_button_left;
-//   }
-
-//   else if ((id.x > 800) && (-400 < id.y < 400) && (id.z < 800)) {
-//     Button_Status = status_read_button_right;
-//   }
-
-//   else if ((id.x < -400) && (id.y > 400) && (id.z < 800)) {
-//     Button_Status = status_read_button_back;
-//   }
-
-//   else if ((id.x > 400) && (id.y > 400) && (id.z < 800)) {
-//     Button_Status = status_read_button_option;
-//   }
-
-//   else if ((id.x > 400) && (id.y < -400) && (id.z < 800)) {
-//     Button_Status = status_read_button_play_pause;
-//   }
-//   delay__ms(100); // give time for less sentive
-
-//   return Button_Status;
-// }
