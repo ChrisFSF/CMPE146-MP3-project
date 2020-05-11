af19a17bc9d2a3ffb2ea2cf72e9dd1bc562bdf70
Chi-Ho <wch1923@hotmail.com>
2020-05-10 21:34:30 -0700

    Update accel_control.c

--- sjtwo-c/projects/lpc40xx_freertos/l5_application/accel_control.c
+++ sjtwo-c/projects/lpc40xx_freertos/l5_application/accel_control.c
@@ -1,4 +1,5 @@
 #include "accel_control.h"
+#include <stdbool.h>
 
 #define OLED__ENABLE_DEBUGGING 0
 
@@ -16,32 +17,39 @@
 
 uint8_t acc_interpret_input(uint8_t Button_Status) {
   acceleration__axis_data_s id = acceleration__get_data();
-  if (id.z < 950) { // when tilted more than 30 degree, 1023 * cos(30) = 886
-    if (-350 < id.x && id.x < 350) {
-      if (350 < id.y && id.y < 1050) {
+  bool is_X_stable = (-350 < id.x && id.x < 350);
+  bool is_Y_stable = (-350 < id.y && id.y < 350);
+  bool is_X_positive = (350 < id.x && id.x < 1050);
+  bool is_X_negative = (-1050 < id.x && id.x < -350);
+  bool is_Y_positive = (350 < id.y && id.y < 1050);
+  bool is_Y_negative = (-1050 < id.y && id.y < -350);
+
+  if (id.z < 950) {
+    if (is_X_stable) {
+      if (is_Y_positive) {
         Button_Status = status_read_button_up;
         OLED__DEBUG_PRINTF("ACC_Up\n");
-      } else if (-1050 < id.y && id.y < -350) {
+      } else if (is_Y_negative) {
         Button_Status = status_read_button_down;
         OLED__DEBUG_PRINTF("ACC_Down\n");
       }
-    } else if (-350 < id.y && id.y < 350) {
-      if (350 < id.x && id.x < 1050) {
+    } else if (is_Y_stable) {
+      if (is_X_positive) {
         Button_Status = status_read_button_right;
         OLED__DEBUG_PRINTF("ACC_Right\n");
-      } else if (-1050 < id.x && id.x < -350) {
+      } else if (is_X_negative) {
         Button_Status = status_read_button_left;
         OLED__DEBUG_PRINTF("ACC_Left\n");
       }
-    } else if (350 < id.y && id.y < 1050) {
-      if (350 < id.x && id.x < 1050) {
+    } else if (is_Y_positive) {
+      if (is_X_positive) {
         Button_Status = status_read_button_option;
         OLED__DEBUG_PRINTF("ACC_Option\n");
-      } else if (-1050 < id.x && id.x < -350) {
+      } else if (is_X_negative) {
         Button_Status = status_read_button_back;
         OLED__DEBUG_PRINTF("ACC_Exit\n");
       }
-    } else if (350 < id.x && id.x < 1050 && -1050 < id.y && id.y < -350) {
+    } else if (is_X_positive && is_Y_negative) {
       Button_Status = status_read_button_play_pause;
       OLED__DEBUG_PRINTF("ACC_Play\n");
     }
